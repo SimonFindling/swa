a1d4dd25571c250233a6fa09ab2f421156e491a1
Maik Scheske <dydress(at)googlemail.com>
2013-06-02 15:08:45 +0200

    - createMethoden in Artikelcontroller hinzugefügt - Grundgerüste einiger benötigter xhtml-Files angelegt(sind noch nicht alle) - messages für die Outputlinks auf deutsch und englisch ergänzt, damit sieht die seite zumindest auf der ersten Blick wieder gut aus :) Notiz: ! Die Links sind weiterhin noch nicht einsatzfähig !

--- shop/src/main/java/de/shop/artikelverwaltung/controller/ArtikelController.java
+++ shop/src/main/java/de/shop/artikelverwaltung/controller/ArtikelController.java
@@ -1,11 +1,16 @@
 package de.shop.artikelverwaltung.controller;
 
+import static de.shop.util.Constants.JSF_REDIRECT_SUFFIX;
+import static javax.ejb.TransactionAttributeType.REQUIRED;
+
 import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
 import java.util.List;
+import java.util.Locale;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.TransactionAttribute;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.Flash;
 import javax.inject.Inject;
@@ -16,6 +21,12 @@ import org.jboss.logging.Logger;
 
 import de.shop.artikelverwaltung.domain.Artikel;
 import de.shop.artikelverwaltung.service.ArtikelService;
+import de.shop.artikelverwaltung.service.ArtikelValidationException;
+import de.shop.kundenverwaltung.domain.Adresse;
+import de.shop.kundenverwaltung.domain.Kunde;
+import de.shop.kundenverwaltung.service.EmailExistsException;
+import de.shop.kundenverwaltung.service.InvalidKundeException;
+import de.shop.util.Client;
 import de.shop.util.Log;
 import de.shop.util.Transactional;
 
@@ -31,14 +42,20 @@ public class ArtikelController implements Serializable {
 
 	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass());
 	
+	private static final String JSF_ARTIKELVERWALTUNG = "/artikelverwaltung/";
 	private static final String JSF_LIST_ARTIKEL = "/artikelverwaltung/listArtikel";
+	private static final String JSF_VIEW_ARTIKEL = JSF_ARTIKELVERWALTUNG + "viewArtikel"; 
 	private static final String FLASH_ARTIKEL = "artikel";
 	private static final int ANZAHL_LADENHUETER = 5;
 	
 	private static final String JSF_SELECT_ARTIKEL = "/artikelverwaltung/selectArtikel";
 	private static final String SESSION_VERFUEGBARE_ARTIKEL = "verfuegbareArtikel";
+	
+	private Artikel artikel;
+	private Artikel neuerArtikel;
 
 	private String bezeichnung;
+	private Long artikelId;
 	
 	private List<Artikel> ladenhueter;
 
@@ -50,6 +67,10 @@ public class ArtikelController implements Serializable {
 	
 	@Inject
 	private transient HttpSession session;
+	
+	@Inject
+	@Client
+	private Locale locale;
 
 	
 	@PostConstruct
@@ -74,7 +95,14 @@ public class ArtikelController implements Serializable {
 	public void setBezeichnung(String bezeichnung) {
 		this.bezeichnung = bezeichnung;
 	}
+	
+	public Artikel getNeuerArtikel() {
+		return neuerArtikel;
+	}
 
+	public void setNeuerArtikel(Artikel neuerArtikel) {
+		this.neuerArtikel = neuerArtikel;
+	}
 
 	public List<Artikel> getLadenhueter() {
 		return ladenhueter;
@@ -92,6 +120,35 @@ public class ArtikelController implements Serializable {
 	@Transactional
 	public void loadLadenhueter() {
 		ladenhueter = as.ladenhueter(ANZAHL_LADENHUETER);
+	}
+	
+	@Transactional
+	public void createEmptyArtikel() {
+		if (neuerArtikel != null) {
+			return;
+		}
+
+		neuerArtikel = new Artikel();
+	}
+	
+	@TransactionAttribute(REQUIRED)
+	@Transactional
+	public String createArtikel() {
+		//try {
+			neuerArtikel = as.createArtikel(neuerArtikel, locale);
+		//TODO Fehlerbehandlung implementieren
+			/*}
+		catch (ArtikelValidationException e) {
+			final String outcome = createArtikelErrorMsg(e);
+			return outcome;
+		}*/
+		
+		// Aufbereitung fuer viewArtikel.xhtml
+		artikelId = neuerArtikel.getId();
+		artikel = neuerArtikel;
+		neuerArtikel = null;  // zuruecksetzen
+		
+		return JSF_VIEW_ARTIKEL + JSF_REDIRECT_SUFFIX;
 	}
 	
 	@Transactional
